3acca33ee7e3d0a0bafd797def5bf68bd0c28e39
Arturs Razmuss <arturs.razmuss@gmail.com>
2024-02-22 17:28:23 +0200

    Initial commit

---
+++ src/main/java/com/arpc/aitests/AiTestsApplication.java
@@ -0,0 +1,13 @@
+package com.arpc.aitests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTestsApplication.class, args);
+	}
+
+}
